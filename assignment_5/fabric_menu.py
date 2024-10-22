ea670b065a0bbe7f70d4506043eabbe5b31085e9
hprell1809 <hprell@ad.unc.edu>
2024-10-21 20:04:55 -0400

    Add files via upload

---
+++ assignment_5/fabric_menu.py
@@ -0,0 +1,23 @@
+#prime loop
+menu_option = ''
+
+#outer loop continues looping until user inputs 'q'
+while menu_option != 'e':
+    #prints menu options
+    print('''
+    Menu:
+    a: browse sewing classes
+    b: view fabric collection
+    e: exit
+    ''')
+    menu_option = input('enter a letter to continue to your desired information: ')
+
+    if menu_option == 'a':
+        print('Classes are organized by age, skill level, and project type!')
+    elif menu_option =='b':
+        quilt_or_clothe = input ('Do you prefer quilting or sewing clothes? Select q for quilting and c for clothing: ')
+        if quilt_or_clothe == 'q':
+            print("Welcome! Please browse our extensive collection of cottons and batiks for your next quilt!")
+        else:
+            print("Welcome! Please browse our collection of cottons, special occasion fabrics, and speciality prints")
+    
