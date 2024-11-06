1c3adf09eeffbdf7bf0c8795b2b19f2a2bf21d2f
hprell <hprell@unc.edu>
2024-11-05 21:04:22 -0500

    remove roster variable

--- assignment_07/roster.py
+++ assignment_07/roster.py
@@ -1,10 +1,9 @@
 # Data taken from: https://goheels.com/sports/mens-basketball/roster
 import pandas as pd
 
-roster = ["Claude", "Brown", "Cadeau", "Davis", "Tyson", "Davis", "Trimble", "Powell", "Jackson", "Washington", "Hawkins"]
-player = {"Last Name": roster,
+player = {"Last Name": ["Claude", "Brown", "Cadeau", "Davis", "Tyson", "Davis", "Trimble", "Powell", "Jackson", "Washington", "Hawkins"],
           "First Name": ["Ty", "James", "Elliot", "RJ", "Cade", "Elijah", "Seth", "Drake", "Ian", "Jalen", "Russell"],
-          "height": [79, 80, 73, 72, 79, 75, 75, 78, 76, 82, 73],
-          "weight": [230, 215, 180, 180, 200, 215, 195, 195, 190, 235, 175]}
+          "Height": [79, 80, 73, 72, 79, 75, 75, 78, 76, 82, 73],
+          "Weight": [230, 215, 180, 180, 200, 215, 195, 195, 190, 235, 175]}
 data = pd.DataFrame(player)
 print(data)
