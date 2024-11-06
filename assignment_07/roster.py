e158c4300c7e34295a912f0198828831cfab73c0
hprell <hprell@unc.edu>
2024-11-05 20:51:53 -0500

    header

--- assignment_07/roster.py
+++ assignment_07/roster.py
@@ -2,5 +2,6 @@
 import pandas as pd
 
 roster = ["Claude", "Brown", "Cadeau", "Davis", "Tyson", "Davis", "Trimble", "Powell", "Jackson", "Washington", "Hawkins"]
+player = {"Last Name": roster}
 data = pd.DataFrame(roster)
 print(data)
